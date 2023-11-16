2796322aae332f068dafe9e521edfaeaacad81ba
morgan176 <113128133+morgan176@users.noreply.github.com>
2023-11-16 18:21:24 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<p></p>
+<p> Group members: Robert Martin, Cassie Allard, Morgan Clapsaddle
+
+<p>
+<br>
+<input name="file" type="file" id="file" size="80"> <br>
+
+<input type="submit" id="u_button" name="u_button" value="Upload the File">
+
+</p>
+<?php
+
+        $file_result = "";
+
+        if ($_FILES["file"]["error"] > 0)
+        {
+        $file_result .= "No File Uploaded or Invalid File ";
+        $file_result .= "Error Code: " . $_FILES["file"]["error"] . "<br>";
+        } else {
+
+        $file_result .=
+        "Upload: " . $_FILES["file"]["name"] . "<br>" .
+        "Type: " . $_FILES["file"]["type"] . "<br>" .
+        "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br>" .
+        "Temp files: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+        move_uploaded_file($_FILES ["file"]["tmp_name"],
+        "/full/path/on/server/" . $_FILES["file"]["name"]);
+
+        $file_result .= "File Upload Successful!";
+        }
+
+
+
+
+ ?>
+ </body >
+ </html >
