5d7fec8293c6b98eb14724688279b98b890394ba
morgan176 <113128133+morgan176@users.noreply.github.com>
2023-11-20 18:05:43 -0500

    Morgan - product.php

---
+++ Morgan - product.php
@@ -0,0 +1,24 @@
+$sql = "SELECT id, username, email, img FROM users";
+$result = $db->query($sql);
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        print "<br> id: ". $row["id"]. "<br> - Name: ". $row["username"]. "<br> - Email: " . $row["email"] . "<br>";
+      print "<img src=\"".$row["img"]."\">";
+
+    }
+} else {
+    print "0 results";
+}
+
+
+
+$db->close();
+        ?>
+
+
+
+</body>
+</html>
