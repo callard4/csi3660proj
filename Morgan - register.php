6b5e34367b0abfcca440d71fec0168372b32b4d9
morgan176 <113128133+morgan176@users.noreply.github.com>
2023-11-20 18:09:34 -0500

    Morgan - register.php

---
+++ Morgan - register.php
@@ -0,0 +1,24 @@
+          <label>Email</label>
+          <input type="email" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+          <label>Password</label>
+          <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+          <label>Confirm password</label>
+          <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+          <button type="submit" class="btn" name="reg_user">Register</button>
+        </div>
+        <p>
+                Already a member? <a href="login.php">Sign in</a>
+        </p>
+        <p></p>
+        <p>
+                Project information page <a href="projectinfo.php" > project information </a>
+        </p>
+  </form>
+</body>
+</html>
