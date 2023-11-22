f766c5d044dcf000ff755c28c91ab91e45a1d26a
morgan176 <113128133+morgan176@users.noreply.github.com>
2023-11-22 13:45:20 -0500

    Create Combined - projectinfo.php

---
+++ Combined - projectinfo.php
@@ -0,0 +1,20 @@
+ <title >CSI3660 Project Webpage </title >
+ </head >
+ <body style="background-color:#33475b" >
+ <h1>CSI3660 Project Information </h1>
+<p>Project Summary: A web server for sharing files, you can upload new files and look through the files that have been uploaded previously. </p>
+<p></p>
+<p>BackGroud: We created a web server where a person can create an account to upload files. </p>
+<p></p>
+<p>Project Overview: Our service is a web server to upload and retrive files. We used php and htlm to make a web site that allows users to cretae an account to save their files and access them later. We save the user profiles and files in a database in phpmyadmin.</p>
+<p></p>
+<p> Group members: Robert Martin, Cassie Allard, Morgan Clapsaddle </p>
+<p></p>
+<p>Screenshots:</p>
+<p></p>
+<p> References: </P>
+
+Already a member? <a href="login.php">Sign in</a>
+
+ </body >
+ </html >
