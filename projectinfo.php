24dae8508f6df4abc3ab74830ae53f35f73b918e
callard4 <150471746+callard4@users.noreply.github.com>
2023-12-03 18:31:34 -0500

    Cassie projectinfo.php

---
+++ projectinfo.php
@@ -0,0 +1,46 @@
+<title >CSI3660 Project Webpage </title >
+</head >
+<body style="background-color:#33475b" >
+ <h1>CSI3660 Project Information </h1>
+<p>Project Summary:
+</p>
+<p>
+Files4U is a web server for sharing files. You can create your own account where you have the option to upload new files or look through the files that have been uploaded previously. </p>
+</p>
+<p>Backgroud:
+</p>
+<p>
+Files4U is a web server where a person can create an account to upload files. We used PHPmyadmin as our database to store account information and files. We also have admin credentials that we use to see user information and history that is restricted from normal user privledges.  </p>
+</p>
+<p>Project Overview:</p>
+
+<p>
+<p>
+Our service is a web server to upload and retrive files. We used php and htlm to make a web site that allows users to cretae an account to save their files and access them later. We save the user profiles and files in a database in phpmyadmin.</p>
+
+<p> Group members:
+</p>
+<p> 
+Robert Martin:
+Helped with...
+</p>
+<p>
+Morgan Clapsaddle:
+Helped with...
+</p>
+<p>
+Cassie Allard:
+Helped with...
+
+ </p>
+<p></p>
+<p>Screenshots:</p>
+<p></p>
+<p> References: </P>
+https://www.sqlshack.com/how-to-backup-and-restore-mysql-databases-using-the-mysqldump-command/
+<p></p>
+  
+Already a member? <a href="login.php">Sign in</a>
+
+ </body >
+ </html >
