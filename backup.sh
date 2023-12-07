2d1df8a0a02f9294534991b9f0731576c22dbb3f
callard4 <150471746+callard4@users.noreply.github.com>
2023-12-07 18:31:37 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-current_dateandtime=$(date +"%Y%m%d_%H%M%S")
 
-db_user="root"
-db_password=""
-db_name=""
+backup_dir="/usr/local/CSI3660ProjectBackup"
+backup_filename="backup_$(date +"%Y%m%d_%H%M%S").tar.gz"
+tar -czvf "$backup_dir/$backup_filename" /var/www/html
 
-backup_dir=""
 
-mysqldump -u"$db_user" -p"$db_password" "$db_name" > "$backup_dir/backup_$current_datetime.sql"
+find "${backup_dir}" -name "backup_*.tar.gz" -type f -mtime +7 -exec rm {} \;
