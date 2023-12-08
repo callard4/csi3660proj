e1a10007d2f3c5eec0ac5d72ddb932d139ff19ca
callard4 <150471746+callard4@users.noreply.github.com>
2023-12-07 19:04:11 -0500

    Update projectinfo.php

--- projectinfo.php
+++ projectinfo.php
@@ -1,6 +1,6 @@
 <title >CSI3660 Project Webpage </title >
 </head >
-<body style="background-color:#33475b" >
+<body style="background-color:#90EE90" >
  <h1>CSI3660 Project Information </h1>
 <p>Project Summary:
 </p>
@@ -22,19 +22,28 @@ Our service is a web server to upload and retrive files. We used php and htlm to
 </p>
 <p> 
 Robert Martin:
-Helped with...
+I worked on the database log as well as the PHP scripts to register, login, and upload the file.
 </p>
 <p>
 Morgan Clapsaddle:
-Helped with...
+I mainly worked on the PHP files and the database. Specifically, I worked on the index, errors, product, server, and style files. I installed phpMyAdmin and created a table to hold all of the data for the users. I created an admin group that only specific users have privledges to, that has access to all files. The other group I created for normal users does not have access to all files. 
 </p>
 <p>
 Cassie Allard:
-Helped with...
+I created the backup strategy and completed the overview webpage for our project. I worked on a script that automatically backs up the files. Using crontab, I was able to set the backup to be at 11 p.m. every night. I also have the system delete backup files after 7 days to help with storage. 
 
  </p>
 <p></p>
 <p>Screenshots:</p>
+<p></p>
+ <img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182465484008992838/image.png?ex=6584cbcc&is=657256cc&hm=b79cc9133ce77ffecc6d6dd15ec9c7aecfb24ccc7d65235a48e5ff25fdde1900&" width="400" height="300">
+<img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182465546885812334/image.png?ex=6584cbdb&is=657256db&hm=3a0eff8a4c50d2d1e97ac91b7ee3a78847b791003fcd4b75ebc3e1bdefdb69a5&" width="400" height="300">
+<p></p>
+<img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182465606067429386/image.png?ex=6584cbe9&is=657256e9&hm=da4c0b188297c117dc40172ba858476d9a3f4c0eb2094a48bb3c61be8acee03a&" width="400" height="300">
+<img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182465689508917328/image.png?ex=6584cbfd&is=657256fd&hm=5a52037defc0d4f8a1b50b7c03c44a0fdd81d4c367f98991a864c2d3f3eb4891&" width="400" height="300">
+<p></p>
+<img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182465921969832036/image.png?ex=6584cc34&is=65725734&hm=bef3ea6d062fc2331c796d2e9921a4c9c56088e355bd074471cd64a89a50b516&" width="400" height="300">
+<img src="https://cdn.discordapp.com/attachments/1164245498346098708/1182466001850335232/image.png?ex=6584cc47&is=65725747&hm=b6404a735c7eef5ac209de901fc1a105f0e75d0d4ce0ff55c8b12a8012ab23e1&" width="500" height="300">
 <p></p>
 <p> References: </P>
 <li>https://www.sqlshack.com/how-to-backup-and-restore-mysql-databases-using-the-mysqldump-command/</li>
